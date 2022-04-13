3b355131cb8693440cd5306929492e442e962cd0
Green_Lab <greencoder001@outlook.com>
2022-04-13 16:28:20 +0200

    Authentification.store() & Authentification.load()

--- huev2.js
+++ huev2.js
@@ -1,9 +1,27 @@
-const discover = require('./src/discover')
-const getAuthentification = require('./src/getAuthentification')
-
+/**
+ * @description Every needed function/Class for the Hue API
+ * @module huev2
+ * @version 1.0.0
+ * @author greencoder001
+ * @license MIT
+ * @returns {Object} Hue API
+ * @example
+    {
+        discover,
+        forceAuth,
+        getAuthentification,
+        isLinkButtonPressed,
+        types: {
+            Authentification,
+        }
+    }
+ */
 module.exports = {
-    discover,
-    getAuthentification,
+    discover: require('./src/discover'),
+    getAuthentification: require('./src/getAuthentification'),
     isLinkButtonPressed: require('./src/isLinkButtonPressed'),
-    forceAuth: require('./src/forceAuth')
+    forceAuth: require('./src/forceAuth'),
+    types: {
+        Authentification: require('./src/types/Authentification')
+    }
 }
--- src/types/Authentification.js
+++ src/types/Authentification.js
@@ -3,6 +3,27 @@ class Authentification {
         this.data = data
         this.error = error
     }
+
+    /**
+     * @description Returns the authentification data as JSON (String)
+     * @returns {String} Stored authentification data
+     */
+    store () {
+        if (this.error) {
+            throw new Error('Authentification.store() can only be called if there is no error')
+        }
+
+        return JSON.stringify(this.data)
+    }
+}
+
+/**
+ * @description Creates new authentification credentials from the given data
+ * @param {String} data Stored Authentification data (from: Authentification.store())
+ * @returns {Authentification} Authentification credentials from the given data
+ */
+Authentification.from = function (data) {
+    return new Authentification(JSON.parse(data), null)
 }
 
 module.exports = Authentification
